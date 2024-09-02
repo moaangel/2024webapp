93ba37be42fa00ab10203106d49b36328f8a815b
Song JiWoong <016vhsdkdl@gmail.com>
2024-09-02 16:50:45 +0900

    vue1

---
+++ 03vue/vuetest/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="container secWrap">
+      <img src="./assets/images/hansome.jpg" alt="" />
+      <div class="card">
+         <h1>{{ title }}</h1>
+         <p>❤️ {{ num }}</p>
+         <p>대여 : {{ won }}</p>
+      </div>
+      {{ todos[1] }}
+      <div v-for="(item, index) in todos" v-bind:key="index">{{ item }}</div>
+      <div v-for="(item, i) in todos" :key="i">{{ item }}</div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'App',
+   data() {
+      return {
+         title: '핸섬가이즈',
+         num: '8.6',
+         won: '11,000',
+         todos: ['서울', '부산', '대구'],
+      };
+   },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/css/style.scss';
+.secWrap {
+   padding: 5px 16px;
+}
+</style>

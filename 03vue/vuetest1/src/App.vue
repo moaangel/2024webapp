727a33d8409d6b13e87703b54b97339b1bffa76a
Song JiWoong <016vhsdkdl@gmail.com>
2024-09-03 12:41:46 +0900

    vue#3

---
+++ 03vue/vuetest1/src/App.vue
@@ -0,0 +1,97 @@
+<template>
+   <div class="container">
+      <h2>HOT & NEW</h2>
+      <div class="cardWrap">
+         <div class="card" v-for="(item, i) in data" v-bind:key="i">
+            <div class="imgWrap">
+               <img :src="`./images/${item.img}`" alt="`${item.img}`" />
+               <div class="hot" v-if="item.like >= 5 ? true : false">HOT</div>
+            </div>
+            <div class="cardBody">
+               <h4>{{ item.title }}</h4>
+               <p>❤️ {{ item.num }}</p>
+               <p>대여 : {{ item.won }}</p>
+               <p>👍 {{ item.like }} <button @:click="increseLike(i)">클릭</button></p>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+import mdata from '@/assets/mdata';
+
+export default {
+   name: 'appView',
+   data() {
+      return {
+         data: mdata,
+      };
+   },
+   methods: {
+      increseLike(i) {
+         console.log(i);
+         this.data[i].like += 1;
+      },
+   },
+};
+</script>
+
+<style lang="scss" scoped>
+$radius: 5px;
+.container {
+   width: 1000px;
+   margin: 0 auto;
+   @media screen and (max-width: 790px) {
+      width: 100%;
+      padding: 0 16px;
+   }
+   img {
+      display: block;
+   }
+}
+.cardWrap {
+   display: flex;
+   flex-wrap: wrap;
+   gap: 10px;
+   .card {
+      width: calc((100% - 30px) / 4);
+
+      @media screen and (max-width: 790px) {
+         width: calc((100% - 10px) / 2);
+      }
+      @media screen and (max-width: 560px) {
+         width: 100%;
+      }
+
+      .imgWrap {
+         width: 100%;
+         position: relative;
+         overflow: hidden;
+         border-radius: $radius;
+         cursor: pointer;
+         img {
+            width: 100%;
+            transition: 0.3s;
+         }
+
+         &:hover img {
+            transform: scale(1.1);
+         }
+         .hot {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            background: skyblue;
+            padding: 2px 10px;
+            border-radius: 5px;
+            font-weight: bold;
+            color: white;
+         }
+      }
+   }
+}
+.mb10 {
+   margin-bottom: 100px !important;
+}
+</style>

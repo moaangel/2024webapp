352a4628717722dda03505ca4cd6eee69e8a4858
Song JiWoong <016vhsdkdl@gmail.com>
2024-09-20 16:20:22 +0900

    todo라우터, 훅

--- 05tododev/front/src/App.jsx
+++ 05tododev/front/src/App.jsx
@@ -3,11 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import root from "./router/root";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={root} />
-    </>
-  );
+  return <RouterProvider router={root} />;
 }
 
 export default App;

f75aab44f8f791aeafe2889102869403eda85f63
Song JiWoong <016vhsdkdl@gmail.com>
2024-09-20 09:15:08 +0900

    todoproject

--- 04react/reactaxio/src/App.js
+++ 04react/reactaxio/src/App.js
@@ -1,21 +1,73 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import LayoutComp from "./layout/LayoutComp";
-import HomeComp from "./components/home/HomeComp";
-import AboutComp from "./components/AboutComp";
-import ProductComp from "./components/ProductComp";
+import React, { useEffect, useState } from "react";
+import axois from "axios";
 
 function App() {
+  const [totalPage, setTotalPage] = useState(0);
+  const [data, setData] = useState([]);
+  const listCnt = 10;
+  const pagerCnt = 5;
+  const [currentpage, setCurrentpage] = useState(1);
+
+  useEffect(() => {
+    api();
+  }, []);
+
+  const api = () => {
+    axois.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setData(res.data);
+      console.log(res);
+      setTotalPage(res.data.length / listCnt);
+    });
+  };
+
+  const pageViewNumber = () => {
+    const pagerNumbers = [];
+    const startPage = Math.floor(((currentpage - 1) / pagerCnt) * pagerCnt + 1);
+    const endPage = Math.min(startPage + pagerCnt - 1, totalPage);
+
+    for (let i = startPage; i <= endPage; i++) {
+      pagerNumbers.push(
+        <span
+          onClick={() => {
+            setCurrentpage(i);
+          }}
+          style={{
+            cursor: "pointer",
+            margin: "0 5px",
+            color: currentpage === i ? "red" : "black",
+          }}
+        >
+          {i}
+        </span>
+      );
+    }
+  };
+
+  const currentPageData = () => {
+    const startIndex = (currentpage - 1) * listCnt;
+    const endIndex = startIndex + listCnt;
+
+    return data.slice(startIndex, endIndex);
+  };
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LayoutComp />}>
-          <Route path="/" element={<HomeComp />} />
-          <Route path="/about" element={<AboutComp />} />
-          <Route path="/products" element={<ProductComp />} />
-        </Route>
-      </Routes>
-    </>
+    <div>
+      <div>
+        <h2>AXIOSSTUDY</h2>
+        <p>TOTALPAGE : {totalPage}</p>
+      </div>
+      {currentPageData().map((item, i) => {
+        return (
+          <div key={i}>
+            {item.id} : {item.title}
+          </div>
+        );
+      })}
+      <div style={{ fontSize: "2em" }}>{pageViewNumber()}</div>
+
+      <button>이전</button>
+      <button>다음</button>
+    </div>
   );
 }
 
